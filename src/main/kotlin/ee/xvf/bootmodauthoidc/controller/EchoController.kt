d98ea06a7f95541a9fc21b19a18e8d4670831b53
kspar <kaspar.papli@gmail.com>
2018-12-06 10:22:36 +0200

    actuator to manual header echo

--- src/main/kotlin/ee/xvf/bootmodauthoidc/controller/EchoController.kt
+++ src/main/kotlin/ee/xvf/bootmodauthoidc/controller/EchoController.kt
@@ -2,15 +2,16 @@ package ee.xvf.bootmodauthoidc.controller
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import javax.servlet.http.HttpServletRequest
 
 @RestController
 class EchoController {
 
     @GetMapping("/")
-    fun echo(x: Any): String {
+    fun echo(req: HttpServletRequest): String {
 
-        return "lul"
-
+        return req.headerNames.toList()
+                .joinToString("<br/>") { "$it : ${req.getHeader(it)}" }
     }
 
 }
