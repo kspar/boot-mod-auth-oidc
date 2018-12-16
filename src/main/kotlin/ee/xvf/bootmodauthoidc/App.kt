555cb9933c6973c59d08a5ff9548b95f394b4b7a
kspar <kaspar.papli@gmail.com>
2018-12-16 23:41:49 +0200

    spring security (pre)authentication using oidc_ headers

--- src/main/kotlin/ee/xvf/bootmodauthoidc/App.kt
+++ src/main/kotlin/ee/xvf/bootmodauthoidc/App.kt
@@ -2,10 +2,112 @@ package ee.xvf.bootmodauthoidc
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AbstractAuthenticationToken
+import org.springframework.security.authentication.AuthenticationProvider
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.web.authentication.preauth.RequestHeaderAuthenticationFilter
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
+import javax.servlet.FilterChain
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
 @SpringBootApplication
 class App
 
 fun main(args: Array<String>) {
     runApplication<App>(*args)
+}
+
+@Configuration
+@EnableWebSecurity(debug = true)
+@EnableGlobalMethodSecurity(securedEnabled = true)
+class SecurityConfig : WebSecurityConfigurerAdapter() {
+    override fun configure(http: HttpSecurity) {
+        http.authorizeRequests().anyRequest().authenticated()
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        http.addFilterAfter(PreAuthHeaderFilter(), RequestHeaderAuthenticationFilter::class.java)
+        http.csrf().disable()
+    }
+
+    override fun configure(auth: AuthenticationManagerBuilder) {
+        auth.authenticationProvider(EasyAuthProvider())
+    }
+}
+
+class PreAuthHeaderFilter : OncePerRequestFilter() {
+    override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, filterChain: FilterChain) {
+        val email = getRequiredHeader("oidc_claim_email", request)
+        val givenName = getRequiredHeader("oidc_claim_given_name", request)
+        val familyName = getRequiredHeader("oidc_claim_family_name", request)
+
+        val roles = mapHeaderToRoles(getRequiredHeader("oidc_claim_easy_role", request))
+
+        val user = EasyUser(email, givenName, familyName, roles)
+        SecurityContextHolder.getContext().authentication = user
+
+        filterChain.doFilter(request, response)
+    }
+
+    private fun getRequiredHeader(headerName: String, request: HttpServletRequest): String {
+        val headerValue: String? = request.getHeader(headerName)
+        if (headerValue.isNullOrBlank()) {
+            throw RuntimeException("$headerName header not found")
+        } else {
+            return headerValue
+        }
+    }
+
+    private fun mapHeaderToRoles(rolesHeader: String): Set<EasyGrantedAuthority> =
+            rolesHeader.split(",")
+                    .map {
+                        when (it) {
+                            "student" -> EasyGrantedAuthority(EasyRole.STUDENT)
+                            "teacher" -> EasyGrantedAuthority(EasyRole.TEACHER)
+                            else -> throw RuntimeException("Unmapped role $it")
+                        }
+                    }
+                    .toSet()
+
+}
+
+class EasyUser(val email: String, val givenName: String, val familyName: String, val roles: Set<EasyGrantedAuthority>) :
+        AbstractAuthenticationToken(roles) {
+
+    // We have no credentials
+    override fun getCredentials(): Any? = null
+
+    override fun getPrincipal(): Any = email
+}
+
+
+class EasyGrantedAuthority(private val role: EasyRole) : GrantedAuthority {
+    override fun getAuthority(): String = role.roleWithPrefix
+}
+
+enum class EasyRole(val roleWithPrefix: String) {
+    STUDENT("ROLE_STUDENT"),
+    TEACHER("ROLE_TEACHER")
+}
+
+
+@Component
+class EasyAuthProvider : AuthenticationProvider {
+    override fun authenticate(authentication: Authentication?): Authentication? {
+        authentication?.isAuthenticated = true
+        return authentication
+    }
+
+    override fun supports(authentication: Class<*>?): Boolean =
+        EasyUser::class.java.isAssignableFrom(authentication)
+
 }
--- src/main/kotlin/ee/xvf/bootmodauthoidc/controller/EchoController.kt
+++ src/main/kotlin/ee/xvf/bootmodauthoidc/controller/EchoController.kt
@@ -1,16 +1,27 @@
 package ee.xvf.bootmodauthoidc.controller
 
+import org.springframework.security.access.annotation.Secured
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 import javax.servlet.http.HttpServletRequest
 
 @RestController
 class EchoController {
 
-    @GetMapping("/")
-    fun echo(req: HttpServletRequest): String {
+    @GetMapping("/teacher")
+    @Secured("ROLE_TEACHER")
+    fun echoTeacher(req: HttpServletRequest): String {
 
-        return req.headerNames.toList()
+        return req.headerNames.toList().union(Arrays.asList("TEACHER"))
+                .joinToString("<br/>") { "$it : ${req.getHeader(it)}" }
+    }
+
+    @GetMapping("/student")
+    @Secured("ROLE_STUDENT")
+    fun echoStudent(req: HttpServletRequest): String {
+
+        return req.headerNames.toList().union(Arrays.asList("STUDENT"))
                 .joinToString("<br/>") { "$it : ${req.getHeader(it)}" }
     }
 
